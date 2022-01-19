3f3367d9516452ef0b8ae2b69fac7a1a8b26e6c0
v1rtsu <90758077+v1rtsu@users.noreply.github.com>
2022-01-19 14:09:48 +0200

    Add files via upload

---
+++ röisivu/script.js
@@ -0,0 +1,15 @@
+let navbar = document.querySelector('.header .navbar');
+let searchForm = document.querySelector('.header .search-form');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+};
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+};
